e2d63adccb371dd6776f173e7f09eb54acf184e2
Mike Stipicevic <stipredirect@gmail.com>
2020-11-15 20:37:43 +0100

    preacty

--- mtv_dl_web/main.py
+++ mtv_dl_web/main.py
@@ -5,6 +5,8 @@ import concurrent.futures
 import json
 import os
 from pathlib import Path
+from quart import abort, jsonify
+from quart import render_template
 
 from mtv_dl import (
     FILMLISTE_DATABASE_FILE,
@@ -55,6 +57,14 @@ async def refresh_database():
             await run_in_sqlite_pool(inner_func)
         REFRESHING_DATABASE.clear()
 
+async def query_database(rules, limit=10):
+    def inner_func():
+        return list(SHOWLIST.filtered(rules=rules, limit=limit))
+
+    return await asyncio.get_running_loop().run_in_executor(
+        SQLITE_POOL_EXECUTOR, inner_func
+    )
+
 
 app = Quart(__name__)
 
@@ -70,24 +80,36 @@ async def refresh_database_on_startup():
 
 @app.after_serving
 async def close_sqlite_pool_executor():
-    await SQLITE_POOL_EXECUTOR.shutdown(wait=True)
+    if shutdownobj := SQLITE_POOL_EXECUTOR.shutdown(wait=True):
+        await shutdownobj
 
+
+@app.route("/query")
+async def query_database():
+    if content is None:
+        abort(400)
+    if LOADING_DATABASE.is_set():
+        abort(422)
+    elif REFRESHING_DATABASE.is_set():
+        abort(422)
+    else:
+        shows = await query_database(request.json['filters'])
+        return jsonify(shows, default=serialize_for_json)
+
+
+
+# @app.route("/info")
+# async def hello():
+#     if LOADING_DATABASE.is_set():
+#         return "loading database"
+#     elif REFRESHING_DATABASE.is_set():
+#         return "refreshing database"
+#     else:
+#         return json.dumps(, default=serialize_for_json, indent=4, sort_keys=True)
 
 @app.route("/")
 async def hello():
-    if LOADING_DATABASE.is_set():
-        return "loading database"
-    elif REFRESHING_DATABASE.is_set():
-        return "refreshing database"
-    else:
-
-        def inner_func():
-            return list(SHOWLIST.filtered(rules=["topic=Magazin"], limit=10))
-
-        shows = await asyncio.get_running_loop().run_in_executor(
-            SQLITE_POOL_EXECUTOR, inner_func
-        )
-        return json.dumps(shows, default=serialize_for_json, indent=4, sort_keys=True)
+    return await render_template('index.html')
 
 
 app.run()
