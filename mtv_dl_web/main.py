6e4c49edddac4af8a8db89a4bff9adfdddbb4cca
Mike Stipicevic <stipredirect@gmail.com>
2020-11-15 20:37:43 +0100

    Basic preact-based query interface

--- mtv_dl_web/main.py
+++ mtv_dl_web/main.py
@@ -6,21 +6,27 @@ import json
 import os
 from pathlib import Path
 
+from asyncache import cached
+from cachetools import LRUCache
 from mtv_dl import (
     FILMLISTE_DATABASE_FILE,
     HISTORY_DATABASE_FILE,
     Database,
     serialize_for_json,
 )
-from quart import Quart
+from paginate import Page
+from quart import Quart, abort, jsonify, render_template, request
 
 SQLITE_POOL_EXECUTOR = concurrent.futures.ThreadPoolExecutor(max_workers=1)
 
-LOADING_DATABASE = asyncio.Event()
-REFRESHING_DATABASE = asyncio.Event()
-DATABASE_LOCK = asyncio.Lock()
+LOADING_DATABASE = None
+REFRESHING_DATABASE = None
+DATABASE_LOCK = None
 
 SHOWLIST = None
+
+DATABASE_QUERY_CACHE = LRUCache(maxsize=32 * 1024)
+DATABASE_QUERY_PAGE_CACHE = LRUCache(maxsize=32 * 1024)
 
 
 async def run_in_sqlite_pool(func):
@@ -41,6 +47,8 @@ async def load_database():
                 )
 
             SHOWLIST = await run_in_sqlite_pool(inner_func)
+            DATABASE_QUERY_CACHE.clear()
+            DATABASE_QUERY_PAGE_CACHE.clear()
         LOADING_DATABASE.clear()
 
 
@@ -56,11 +64,32 @@ async def refresh_database():
         REFRESHING_DATABASE.clear()
 
 
+@cached(cache=DATABASE_QUERY_CACHE)
+def query_database_inner(rules, limit=10000):
+    return list(SHOWLIST.filtered(rules=rules, limit=limit))
+
+
+@cached(cache=DATABASE_QUERY_PAGE_CACHE)
+async def query_database(rules, page, items_per_page=20, limit=10000):
+    def inner_func():
+        return Page(query_database_inner(rules=rules, limit=limit), page=page, items_per_page=items_per_page)
+
+    return await run_in_sqlite_pool(inner_func)
+
+
 app = Quart(__name__)
 
 
 @app.before_serving
 async def refresh_database_on_startup():
+    global LOADING_DATABASE
+    global REFRESHING_DATABASE
+    global DATABASE_LOCK
+
+    LOADING_DATABASE = asyncio.Event()
+    REFRESHING_DATABASE = asyncio.Event()
+    DATABASE_LOCK = asyncio.Lock()
+
     async def inner_func():
         await load_database()
         await refresh_database()
@@ -70,24 +99,69 @@ async def refresh_database_on_startup():
 
 @app.after_serving
 async def close_sqlite_pool_executor():
-    await SQLITE_POOL_EXECUTOR.shutdown(wait=True)
+    if shutdownobj := SQLITE_POOL_EXECUTOR.shutdown(wait=True):
+        await shutdownobj
 
 
-@app.route("/")
-async def hello():
+@app.route("/database_status")
+async def database_status():
     if LOADING_DATABASE.is_set():
         return "loading database"
     elif REFRESHING_DATABASE.is_set():
         return "refreshing database"
     else:
-
-        def inner_func():
-            return list(SHOWLIST.filtered(rules=["topic=Magazin"], limit=10))
-
-        shows = await asyncio.get_running_loop().run_in_executor(
-            SQLITE_POOL_EXECUTOR, inner_func
-        )
-        return json.dumps(shows, default=serialize_for_json, indent=4, sort_keys=True)
+        return "database ready"
 
 
-app.run()
+@app.route("/refresh_database", methods=["POST"])
+async def refresh_database_route():
+    asyncio.create_task(refresh_database())
+    return "refreshing database"
+
+
+@app.route("/query", methods=["POST"])
+async def query():
+    body_json = await request.get_json()
+
+    rules = body_json.get("rules", [])
+    limit = int(body_json.get("limit", 10))
+    page = int(body_json.get("page", 1))
+    if page < 0:
+        abort(400, "page cannot be below 1")
+
+    if LOADING_DATABASE.is_set():
+        return jsonify({"busy": "loading database"})
+    elif REFRESHING_DATABASE.is_set():
+        return jsonify({"busy": "refreshing database"})
+    else:
+        database_lock = DATABASE_LOCK
+        try:
+            await asyncio.wait_for(database_lock.acquire(), timeout=1.0)
+            try:
+                # todo: use jsonify
+                results = await query_database(
+                    tuple(rules), page=page, items_per_page=limit
+                )
+                return json.dumps(
+                    {
+                        "result": results.items,
+                        "last_page": results.last_page,
+                        "item_count": results.item_count,
+                        "page": results.page,
+                    },
+                    default=serialize_for_json,
+                    indent=4,
+                    sort_keys=True,
+                )
+            finally:
+                database_lock.release()
+        except asyncio.TimeoutError:
+            return jsonify({"busy": "performing database operations"})
+
+
+@app.route("/")
+async def hello():
+    return await render_template("index.html")
+
+
+app.run(debug=True)
---
+++ mtv_dl_web/templates/main.js
@@ -0,0 +1,232 @@
+import { html, Component, render } from '/static/js/preact.js';
+
+const debounce = (func, delay) => {
+  let inDebounce
+  return function() {
+    const context = this
+    const args = arguments
+    clearTimeout(inDebounce)
+    inDebounce = setTimeout(() => func.apply(context, args), delay)
+  }
+}
+
+function PageList(props) {
+  function jump_to_page(page) {
+    props.pageListSetState({ page: page, query_filters: props.queryFilters});
+  }
+
+  function render_link(page) {
+    if (props.page == page) {
+      return html`${page} `
+    } else {
+      return html`<a onclick=${() => {jump_to_page(page)}}>${page}</a> `
+    }
+  }
+
+  if (props.pages < 2) {
+    return [];
+  }
+
+  const page_array = [...Array(props.pages).keys()].map(i => {return render_link(i+1)});
+  return page_array
+}
+
+class SearchField extends Component {
+  state = {
+    value: "",
+  }
+
+  onInput = e => {
+    const { value } = e.target;
+    this.setState({ value });
+    var new_filter_state = this.props.oldstate()['query_filters'];
+    new_filter_state[this.props.name] = value;
+    this.props.searchListSetState({ query_filters: new_filter_state })
+  }
+
+  render(props, state) {
+    return (
+      html`
+        <td>
+          <input type="text" value=${state.value} onInput=${this.onInput} />
+        </td>
+      `
+    );
+  }
+}
+
+class SearchList extends Component {
+  state = {
+    list: [],
+    title: "",
+    query_filters: {},
+    queries_in_progress: 0,
+    page: 1,
+    pages: 1,
+    item_count: 0,
+  }
+
+  async queryList(rules, page) {
+    const resultsperpage = this.props.resultsPerPage;
+    async function inner_query() {
+      const rawResponse = await fetch('/query', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          limit: resultsperpage,
+          rules: rules,
+          page: page,
+        })
+      });
+      return await rawResponse.json();
+    }
+    const content = await inner_query();
+    if ('result' in content) {
+      this.setState({
+        list: content['result'],
+        page: content['page'],
+        pages: content['last_page'],
+        item_count: content['item_count'],
+      })
+    }
+  }
+
+  async onSubmit(query_filters, page) {
+    if (Object.values(query_filters).every(x => !x)) {
+      this.setState({item_count: 0, list: [], page:0, pages:0});
+      return;
+    }
+    this.setState({queries_in_progress: 1});
+    const result = Object.entries(query_filters).filter(x => x[1].length > 0).map(x => x[0] + '=' + x[1]);
+    await this.queryList(result, page);
+    this.setState({queries_in_progress: 0});
+  }
+
+  linkHandler(p) {
+    this.setState(p);
+    this.onSubmit(p.query_filters, p.page)
+  }
+
+  inputHandler(p) {
+    this.setState(p);
+    this.onSubmit(p.query_filters, 1);
+  }
+
+  render(props, { value }) {
+    const current_lower_bound = (this.state.item_count == 0) ? 0 : ((this.state.page - 1) * props.resultsPerPage) + 1;
+    const current_upper_bound = Math.min(
+      this.state.item_count,
+      (this.state.page * props.resultsPerPage) + 1);
+
+    return (
+      html`
+      ${(this.state.queries_in_progress > 0) ? html`<progress class="progress is-small is-primary" max="100">15%</progress>` : html`<div></div>`}
+      <p>results found (${current_lower_bound}-${current_upper_bound} out of ${this.state.item_count}) <${PageList} pageListSetState=${p=>{this.linkHandler(p)}} pages=${this.state.pages} page=${this.state.page} queryFilters=${this.state.query_filters}/></p>
+      <table class="table is-striped is-hoverable is-bordered is-fullwidth">
+      <tr>
+        <th>Title</th>
+        <th>Channel</th>
+        <th>Date</th>
+        <th>Duration</th>
+        <th>Topic</th>
+      </tr>
+      <tr>
+        <${SearchField} name="title" oldstate=${()=>{return this.state}} searchListSetState=${p=>{this.inputHandler(p)}} />
+        <${SearchField} name="channel" oldstate=${()=>{return this.state}} searchListSetState=${p=>{this.inputHandler(p)}} />
+        <${SearchField} name="start" oldstate=${()=>{return this.state}} searchListSetState=${p=>{this.inputHandler(p)}} />
+        <${SearchField} name="duration" oldstate=${()=>{return this.state}} searchListSetState=${p=>{this.inputHandler(p)}} />
+        <${SearchField} name="topic" oldstate=${()=>{return this.state}} searchListSetState=${p=>{this.inputHandler(p)}} />
+      </tr>
+      ${this.state.list.map(element => html`
+      <tr>
+        <td>${element['title']}</td>
+        <td>${element['channel']}</td>
+        <td>${element['start']}</td>
+        <td>${element['duration']}</td>
+        <td>${element['topic']}</td>
+      </tr>
+      `)}
+      </table>
+      `
+    );
+  }
+}
+
+class DatabaseStatus extends Component {
+  state = { database_status: "connecting to server" };
+
+  // Called whenever our component is created
+  async componentDidMount() {
+    this.timer = setInterval(async () => {
+      const database_status_fetch = await fetch('/database_status');
+      const status_text = await database_status_fetch.text();
+      // update status every second
+      this.setState({database_status: status_text});
+    }, 1000);
+  }
+
+  // Called just before our component will be destroyed
+  componentWillUnmount() {
+    // stop when not renderable
+    clearInterval(this.timer);
+  }
+
+  async refresh() {
+    this.setState({database_status: "requesting database refresh" });
+    const database_status_fetch = await fetch('/refresh_database', {method: 'POST'});
+    const status_text = await database_status_fetch.text();
+  }
+
+  render(props, state) {
+    return html`<span>${this.state.database_status}</span>`;
+  }
+}
+
+class App extends Component {
+  database_status_ref = null;
+  setDatabaseStatusRef = (dom) => this.database_status_ref = dom;
+
+  addTodo() {
+    const { todos = [] } = this.state;
+    this.setState({ todos: todos.concat(`Item ${todos.length}`) });
+  }
+
+  render(props, state) {
+    return html`
+      <div class="app">
+        <div class="m-2">
+          <div class="columns">
+            <div class="column is-half">
+              <${Header} />
+            </div>
+            <div class="column is-half">
+              <div class="has-text-right">
+                <span class="mr-3">
+                  <${DatabaseStatus} ref=${this.setDatabaseStatusRef} />
+                </span>
+                <button onClick=${() => this.database_status_ref.refresh()}>Refresh Database</button>
+              </div>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column is-full">
+              <${SearchList} resultsPerPage=10 />
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column is-full">
+              <${Footer}>Served by <a href="https://github.com/wickedchicken/mtv_dl_web">mtv_dl_web</a>.<//>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+}
+
+const Header = () => html`<p>Mediathek Search</p>`
+
+const Footer = props => html`<footer ...${props} />`
